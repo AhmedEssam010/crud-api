7aa94d1e85d80ce7a27434a7a26cbdb29b6d6a59
ahmed eldweek <eldweekahmed7@gmail.com>
2026-02-03 11:47:11 +0200

    Added dotenv support and updated index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config(); 
+
 const express = require("express");
 const mongoose = require("mongoose");
 const Product = require("./models/product.model.js");
@@ -6,27 +8,23 @@ const app = express();
 
 // middleware
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-
+app.use(express.urlencoded({ extended: false }));
 
 // routes
 app.use("/api/products", productRoute);
-
-
-
 
 app.get("/", (req, res) => {
   res.send("Hello from Node API Server Updated");
 });
 
-
-mongoose.connect("mongodb+srv://ahmedeldweek:Ahmed215%23%2A@crudapi.uf0w8bp.mongodb.net/Node-API?appName=crudapi")
+// connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Connected to database!");
-    app.listen(4000, () => {
-      console.log("Server is running on port 4000");
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
     });
   })
-  .catch(() => {
-    console.log("Connection failed!");
+  .catch((err) => {
+    console.log("Connection failed!", err);
   });
